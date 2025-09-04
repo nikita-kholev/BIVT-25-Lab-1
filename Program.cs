7a44579d36b0e3e442cdb92866711fc0b44d054f
nikita-kholev <nikitakholev73@yandex.ru>
2025-09-04 17:10:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            White white = new White();
+            Console.WriteLine(white.Task1(1.2));
+            Console.WriteLine(white.Task2(2));
+            Console.WriteLine(white.Task3(2, 3));
+            Console.WriteLine(white.Task4(-2.5, 1.89));
+            Console.WriteLine(white.Task5(0.5));
+            Console.WriteLine(white.Task6(2.5, 1.89, 3));
+            Console.WriteLine(white.Task7(3));
+            Console.WriteLine(white.Task8(2.5, 2, 3));
+        }
+    }
+}
